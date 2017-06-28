8be24f094e25ae80edfb4614ade4779fa7e3dc50
Juan Pablo M. Bastida <jpbastida@hotmail.com>
2017-06-28 19:11:44 +0300

    Car Delegate

---
+++ CarDelegate/Car.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDelegate
+{
+    class Car
+    {
+        public delegate void CarEngineHandler(string msgForCaller);
+
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; }
+        public string PetName { get; set; }
+
+        private bool carIsDead;
+
+        public Car()
+        {
+            MaxSpeed = 100;
+        }
+
+        public Car(string name, int maxSp, int currSp)
+        {
+            CurrentSpeed = currSp;
+            MaxSpeed = maxSp;
+            PetName = name;
+        }
+
+        // Define variable of the Delegate
+        private CarEngineHandler listOfHandlers;
+
+        // Registration functions for the caller
+        public void RegisterWithCarEngine(CarEngineHandler methodToCall)
+        {
+            listOfHandlers = methodToCall;
+        }
+
+        public void Accelerate(int delta)
+        {
+            if (carIsDead)
+            {
+                if (listOfHandlers != null)
+                {
+                    listOfHandlers("You burned the car!");
+                }
+            }
+            else
+            {
+                CurrentSpeed += delta;
+
+                if ((MaxSpeed - CurrentSpeed) == 10 && listOfHandlers != null)
+                {
+                    listOfHandlers("Careful! Gonna blow!!!");
+                }
+
+                if (CurrentSpeed >= MaxSpeed)
+                {
+                    carIsDead = true;
+                }
+                else
+                {
+                    Console.WriteLine($"Current Speed = {CurrentSpeed}");
+                }
+            }
+        }
+    }
+}
--- CarDelegate/Program.cs
+++ CarDelegate/Program.cs
@@ -10,6 +10,24 @@ namespace CarDelegate
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("**** Delegates as event enablers ****");
+
+            Car car = new Car("Jaguar", 100, 10);
+
+            car.RegisterWithCarEngine(new Car.CarEngineHandler(OnCarEngineEvent));
+
+            Console.WriteLine("*** Speeding UP!!! ***");
+            for (int i = 0; i < 6; i++)
+            {
+                car.Accelerate(20);
+            }
+        }
+
+        private static void OnCarEngineEvent(string msgForCaller)
+        {
+            Console.WriteLine("\n **** Message from Car Object ****");
+            Console.WriteLine($"=> {msgForCaller}");
+            Console.WriteLine("*******************************\n ");
         }
     }
 }
